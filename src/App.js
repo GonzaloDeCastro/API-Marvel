158a140a373303cf823ddb7d7e02cf446e2a0112
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2022-06-02 22:52:32 -0300

    API conection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [character, setCharacter] = useState([]);
+	useEffect(() => {
+		axios
+			.get(
+				'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=d2f74ceaed1589cacec81ceec2c61b9e&hash=871be6448964d9fb153bd9eabb671ff1'
+			)
+			.then((res) => {
+				setCharacter(res.data.data.results);
+			})
+			.catch((error) => console.log(error));
+	}, []);
+	console.log(character);
+	return (
+		<div className="App">
+			<h1>Marvel</h1>
+		</div>
+	);
 }
 
 export default App;
